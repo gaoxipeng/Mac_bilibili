168121305f9bd6a9c57d6b91c565f75befd4690b
gaoxipeng <764912315@qq.com>
2026-07-01 01:02:06 +0800

    Initial Commit

---
+++ bilibili/bilibiliApp.swift
@@ -0,0 +1,17 @@
+//
+//  bilibiliApp.swift
+//  bilibili
+//
+//  Created by 高熙鹏 on 2026/7/1.
+//
+
+import SwiftUI
+
+@main
+struct bilibiliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
